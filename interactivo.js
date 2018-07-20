e951fa4023a813e70a5e3d6d205ae86df37e6583
wil3000 <41454611+wil3000@users.noreply.github.com>
2018-07-20 13:43:38 -0500

    master
    mi primera web

---
+++ interactivo.js
@@ -0,0 +1,59 @@
+alert("HOLA \"hermoso\"MUNDO");
+function mostraralerta()
+{    alert('hizo clic!');}
+/*var nom;
+nom=prompt("Escribe tu nombre ","Paco");
+alert("Mucho gusto "+ nom)
+alert("Hola\t"+"que"+"\n"+"tal");*/
+
+/*var num;
+num=parseFloat(prompt("Escribe un número",""));
+if(num==100)
+ {
+ alert("El número que has escrito es 100");
+ }
+else
+ {
+ alert("El número que has escrito no es 100");
+ }
+if(num>0)
+ {
+ alert("El número que has escrito es positivo");
+ }
+else
+ {
+ alert("El número es negativo o 0");
+ }*/
+
+ //bucle while
+//Mientras” se cumpla la condición, se irán repitiendo las sentencias 1, 2 y 3.
+ /*var x=0;
+ while(x<3)
+  {
+  alert("El valor de x es= "+x);
+  x=x+1;
+  }
+ 
+/*var i=2;
+var res="";
+var j=7;
+while (i<j)
+ { res=res+" "+i+" "+j+"\n"; 
+ i++; 
+ j--; } 
+alert(res);*/
+
+//for (contador = valor inicial; condición; expresión de incremento)
+/*En palabras significa: 
+“Desde i=1, hasta i=10 de 1 en 1, repite las sentencias 1, 2 y 3”
+Es decir: Repite 10 veces las sentencias 1, 2 y 3*/
+/*var texto;
+var num;
+var salida="";
+texto=prompt("Escribe un texto","");
+num=prompt("Cuántas veces quieres que lo repita","");
+num=parseInt(num,10);
+for(i=1;i<=num;i++)
+ { salida=salida+texto+"\n"; } 
+ alert(salida);*/
+
